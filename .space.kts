080317f333f4ca50490197f6f6e0e02251d46676
rickdotnet <rick@rickdot.net>
2024-05-11 12:56:42 -0500

    swap to docker hub

--- .space.kts
+++ .space.kts
@@ -1,11 +1,25 @@
-job("Build/Push to Docker registry") {
-    docker {
-        build {
-            file = "./Dockerfile"
+job("Build/Push Momus") {
+    // manually triggered
+    startOn {} 
+    host("Build and push to Docker Hub") {
+	
+        env["D_USER"] = "{{ project:docker-user }}"
+		env["D_TOKEN"] = "{{ project:docker-token }}"
+		env["D_VERSION"] = "{{ project:momus-docker-version }}"
+		
+		shellScript("Login to Docker Hub"){
+            content = """
+                docker login -u ${'$'}D_USER -p ${'$'}D_TOKEN
+            """
         }
-
-        push("{{ project:docker-image }}") {
-            tags("latest")
+        
+        dockerBuildPush  {
+            file = "./Dockerfile"
+            labels["author"] = "rickdotnet"
+            tags {
+                +"rickdotnet/momus:latest"
+                +"rickdotnet/momus:${"$"}D_VERSION.${"$"}JB_SPACE_EXECUTION_NUMBER"
+            }
         }
     }
 }
