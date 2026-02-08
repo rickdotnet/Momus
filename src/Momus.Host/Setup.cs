3443545bea111e13f7661a7dbc03e4cd50d01374
rickdotnet <rick@rickdot.net>
2026-02-07 22:55:54 -0600

    add build and start scripts
    tidy, and things
    ``:
    same shit
    
    lol
    
    add inbox prefix

--- src/Momus.Host/Setup.cs
+++ src/Momus.Host/Setup.cs
@@ -30,24 +30,25 @@ public static class Setup
     {
         private void ConfigureNats()
         {
-            builder.Services.AddNatsClient(nats => nats.ConfigureOptions(optsBuilder =>
-                {
-                    optsBuilder.Configure(natsOpts =>
-                    {
-                        natsOpts.Opts = new NatsOpts
+            builder.Services.AddNatsClient(nats => nats
+                .ConfigureOptions(optsBuilder => optsBuilder
+                    .Configure(opts => opts.Opts = opts.Opts with
                         {
-                            Url = builder.Configuration[nameof(MomusSettings.NatsUrl)] ?? natsOpts.Opts.Url,
+                            Url = builder.Configuration[nameof(MomusSettings.NatsUrl)] ?? opts.Opts.Url,
+                            InboxPrefix = builder.Configuration[nameof(MomusSettings.User)] ?? Guid.NewGuid().ToString()[..8],
                             AuthOpts = new NatsAuthOpts
                             {
+                                Username = builder.Configuration[nameof(MomusSettings.User)],
+                                Password = builder.Configuration[nameof(MomusSettings.Pass)],
                                 CredsFile = builder.Configuration[nameof(MomusSettings.CredsFilePath)],
                                 Jwt = builder.Configuration[nameof(MomusSettings.Jwt)],
                                 NKey = builder.Configuration[nameof(MomusSettings.NKey)],
                                 Seed = builder.Configuration[nameof(MomusSettings.Seed)],
                                 Token = builder.Configuration[nameof(MomusSettings.Token)],
                             }
-                        };
-                    });
-                })
+                        }
+                    )
+                )
             );
 
             builder.Services.AddHostedService<RouteConfigBackgroundService>();
